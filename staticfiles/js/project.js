405a73d679e3340823694abcbce9cc97ea19c899
nachos5 <gnorland93@gmail.com>
2018-10-14 19:43:57 +0000

    production fix11

---
+++ staticfiles/js/project.js
@@ -0,0 +1,21 @@
+/* Project specific Javascript goes here. */
+
+// header nav
+// const headernav = $("#header-nav");
+// const headernavlink = $("#header-nav .nav-link");
+//
+// $(document).ready(function() {
+//   headernavlink.on("click", function (e) {
+//     headernav.find(".nav-link").removeClass("active");
+//     $(this).addClass("active");
+//     // alert("virkar");
+//   })
+// });
+
+// hringekja
+/*if ($(".carousel")) {
+  const $inner = $(".carousel-inner");
+  $inner.each(function() {
+    $(this).children().first().addClass("active");
+  })
+}*/
