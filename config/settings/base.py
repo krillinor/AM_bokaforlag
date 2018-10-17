2a68f3a01c574d6ea0b6f7e2183cfcc4386bc1e8
nachos5 <gnorland93@gmail.com>
2018-10-17 11:36:51 +0000

    123

--- config/settings/base.py
+++ config/settings/base.py
@@ -266,3 +266,13 @@ Configuration.configure(
     BRAINTREE_PRIVATE_KEY
 )
 # ------------------------------------------------------------------------------
+
+# GMAIL DÓT
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'amforlag@gmail.com'
+SERVER_EMAIL = 'amforlag@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'amforlag@gmail.com'
+EMAIL_HOST_PASSWORD = 'AlmaNorland1'
--- config/settings/local.py
+++ config/settings/local.py
@@ -15,16 +15,6 @@ ALLOWED_HOSTS = [
     ".herokuapp.com",
 ]
 
-# GMAIL DÓT
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = 'amforlag@gmail.com'
-SERVER_EMAIL = 'amforlag@gmail.com'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'amforlag@gmail.com'
-EMAIL_HOST_PASSWORD = 'AlmaNorland1'
-
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
