18cfeb7645d78d48fe8c58c0655bb7817f36b371
nachos5 <gnorland93@gmail.com>
2018-11-05 14:28:31 +0000

    fréttir update

--- bokaforlag/frettir/views.py
+++ bokaforlag/frettir/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import Frett
 
 def frettir_view(request):
-    frettir = Frett.objects.all()
+    frettir = Frett.objects.all().order_by('-id')
     return render(request, "frettir/frettir.html",
                   {"frettir_allar": frettir})
